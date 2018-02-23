c97565f30f18eb8cdebd5effaec16ef050cdd502
Chris Bremmer <c_bremmer@outlook.com>
2018-02-23 04:47:41 -0800

    added bouncing barrels

--- js/main.js
+++ js/main.js
@@ -101,6 +101,7 @@ let GameState = {
     this.player.animations.add("walking", [0, 1, 2, 1], 6, true);
     this.game.physics.arcade.enable(this.player);
     this.player.customParams = {};
+    this.player.body.collideWorldBounds = true;
 
     this.game.camera.follow(this.player);
 
@@ -109,6 +110,7 @@ let GameState = {
     this.barrels = this.add.group();
     this.barrels.enableBody = true;
 
+    this.createBarrel();
     this.barrelCreator = this.game.time.events.loop(
       Phaser.Timer.SECOND * this.levelData.barrelFrequency,
       this.createBarrel,
@@ -119,7 +121,15 @@ let GameState = {
     this.game.physics.arcade.collide(this.player, this.ground);
     this.game.physics.arcade.collide(this.player, this.platforms);
 
+    this.game.physics.arcade.collide(this.barrels, this.ground);
+    this.game.physics.arcade.collide(this.barrels, this.platforms);
+
     this.game.physics.arcade.overlap(this.player, this.fires, this.killPlayer);
+    this.game.physics.arcade.overlap(
+      this.player,
+      this.barrels,
+      this.killPlayer
+    );
     this.game.physics.arcade.overlap(this.player, this.goal, this.win);
 
     this.player.body.velocity.x = 0;
@@ -147,6 +157,12 @@ let GameState = {
       this.player.body.velocity.y = -this.JUMPING_SPEED;
       this.player.customParams.mustJump = false;
     }
+
+    this.barrels.forEach(function(element) {
+      if (element.x < 10 && element.y > 600) {
+        element.kill();
+      }
+    }, this);
   },
   createOnscreenControls() {
     this.leftArrow = this.add.button(20, 535, "arrowButton");
@@ -219,7 +235,11 @@ let GameState = {
       barrel = this.barrels.create(0, 0, "barrel");
     }
 
+    barrel.body.collideWorldBounds = true;
+    barrel.body.bounce.set(1, 0);
+
     barrel.reset(this.levelData.goal.x, this.levelData.goal.y);
+    barrel.body.velocity.x = this.levelData.barrelSpeed;
   }
 };
 
