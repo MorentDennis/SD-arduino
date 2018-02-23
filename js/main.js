edab60fb048629f3896a26d2c9a2058c2252c4f6
Chris Bremmer <c_bremmer@outlook.com>
2018-02-23 03:25:28 -0800

    added camera movement

--- js/main.js
+++ js/main.js
@@ -12,6 +12,8 @@ let GameState = {
 
     this.cursors = this.game.input.keyboard.createCursorKeys();
 
+    this.game.world.setBounds(0, 0, 360, 700);
+
     this.RUNNING_SPEED = 180;
     this.JUMPING_SPEED = 550;
   },
@@ -46,7 +48,7 @@ let GameState = {
   },
   //executed after everything is loaded
   create() {
-    this.ground = this.add.sprite(0, 500, "ground");
+    this.ground = this.add.sprite(0, 638, "ground");
     this.game.physics.arcade.enable(this.ground);
     this.ground.body.allowGravity = false;
     this.ground.body.immovable = true;
@@ -69,11 +71,13 @@ let GameState = {
     this.platforms.setAll("body.allowGravity", false);
 
     //create player
-    this.player = this.add.sprite(100, 200, "player", 3);
+    this.player = this.add.sprite(10, 545, "player", 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add("walking", [0, 1, 2, 1], 6, true);
     this.game.physics.arcade.enable(this.player);
     this.player.customParams = {};
+
+    this.game.camera.follow(this.player);
 
     this.createOnscreenControls();
   },
@@ -108,6 +112,10 @@ let GameState = {
     this.leftArrow.alpha = 0.5;
     this.rightArrow.alpha = 0.5;
     this.actionButton.alpha = 0.5;
+
+    this.leftArrow.fixedToCamera = true;
+    this.rightArrow.fixedToCamera = true;
+    this.actionButton.fixedToCamera = true;
 
     this.actionButton.events.onInputDown.add(function() {
       this.player.customParams.mustJump = true;
