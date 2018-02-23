0a2db92c97eac37e026f5d8268b15248de34d6ab
Chris Bremmer <c_bremmer@outlook.com>
2018-02-23 03:04:07 -0800

    added physics and gravity

--- js/main.js
+++ js/main.js
@@ -1,15 +1,18 @@
 //this game will have only 1 state
-var GameState = {
+let GameState = {
   //initiate game settings
-  init: function() {
+  init() {
     //adapt to screen size, fit all the game
     this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
     this.scale.pageAlignHorizontally = true;
     this.scale.pageAlignVertically = true;
+
+    this.game.physics.startSystem(Phaser.Physics.ARCADE);
+    this.game.physics.arcade.gravity.y = 1000;
   },
 
   //load the game assets before the game starts
-  preload: function() {
+  preload() {
     this.load.image("ground", "assets/images/ground.png");
     this.load.image("platform", "assets/images/platform.png");
     this.load.image("goal", "assets/images/gorilla3.png");
@@ -37,22 +40,28 @@ var GameState = {
     );
   },
   //executed after everything is loaded
-  create: function() {
+  create() {
     this.ground = this.add.sprite(0, 500, "ground");
+    this.game.physics.arcade.enable(this.ground);
+    this.ground.body.allowGravity = false;
+    this.ground.body.immovable = true;
 
-    var platform = this.add.sprite(0, 300, "platform");
+    let platform = this.add.sprite(0, 300, "platform");
+    this.game.physics.arcade.enable(platform);
+    platform.body.allowGravity = false;
+    platform.body.immovable = true;
 
     //create player
     this.player = this.add.sprite(100, 200, "player", 3);
     this.player.anchor.setTo(0.5);
     this.player.animations.add("walking", [0, 1, 2, 1], 6, true);
-    this.player.play("walking");
+    this.game.physics.arcade.enable(this.player);
   },
-  update: function() {}
+  update() {}
 };
 
 //initiate the Phaser framework
-var game = new Phaser.Game(360, 592, Phaser.AUTO);
+let game = new Phaser.Game(360, 592, Phaser.AUTO);
 
 game.state.add("GameState", GameState);
 game.state.start("GameState");
