978f31b0aa8d86459090bb1a1797fce4101b0b8f
Senesael Robbe <robbe.senesael@student.howest.be>
2018-05-25 19:26:05 +0200

    added the changes

--- js/main.js
+++ js/main.js
@@ -349,10 +349,44 @@ let GameState = {
   }
 };
 
+let titlescreen;
+let Game = {
+
+// Game.MainMenu = function(game){
+//
+// };
+
+
+
+
+    create: function (game) {
+        this.createButton(game, "Play", game.world.centerX, game.world.centerY + 32, 300, 100, function () {
+            this.state.start("GameState");
+        });
+        titlescreen = game.add.sprite(game.world.centerX, game.world.centerY - 192, "titlescreen");
+        titlescreen.anchor.setTo(0.5, 0.5);
+    },
+    update: function (game) {
+
+    },
+    createButton: function (game, string, x, y, w, h, callback) {
+        let button1 = game.add.button(x, y, 'button', callback, this, 2, 1, 0);
+        button1.anchor.setTo(0.5, 0.5);
+        button1.width = w;
+        button1.height = h;
+        let text = game.add.text(button1.x, button1.y, string, {
+            font: '14px Arial',
+            fill: "#fff", align: "center"
+        });
+        text.anchor.setTo(0.5, 0.5);
+    }
+
+};
+
 //initiate the Phaser framework
 let game = new Phaser.Game(360, 592, Phaser.AUTO);
-
+game.state.add("Game", Game);
 game.state.add("GameState", GameState);
-game.state.start("GameState");
+game.state.start("Game");
 
 
